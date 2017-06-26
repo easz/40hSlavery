2ab7987cff0c60d12f5f78d99911b20bb6a3c361
Tsungi Lyn <easy.lin@gmail.com>
2017-06-26 09:07:35 +0200

    done calendar view

--- charts/lib/demo_data.js
+++ charts/lib/demo_data.js
@@ -18,36 +18,47 @@ var DEMO_DATA = (function () {
     root['days'] = [];
     var children = root['children'] = [];
 
-    var days = d3.timeDays(new Date(2016, 0, 11), new Date(2016, 11, 10));
-    for (var i = 0; i < days.length; i++) {
-      var weekday = days[i].getDay();
-      if (weekday<2 || weekday>6) // only week days
+    var days = d3.timeDays(new Date(2016, 0, 11, 8, 0, 0), new Date(2016, 11, 10, 8, 0, 0));
+    for (var day_i = 0; day_i < days.length; day_i++) {
+      var weekday = days[day_i].getDay();
+      if (weekday < 2 || weekday > 6) // only week days
         continue;
-      if (Math.random() < 0.24) // some days off
+      if (Math.random() < 0.3) // some days off
         continue;
-      const date = days[i].toISOString().substring(0, 10) + "T08:00:00";
-      var node =
-        {
-          name: "firefox",
-          days: [],
-          children: [
+      const date = days[day_i].toISOString().substring(0, 10) + "T08:00:00";
+      for (const app in app_path) {
+        const app_num = Object.keys(app_path).length;
+        if (Math.random() < 0.2) 
+          continue;
+        const app_time = (7*3600 / app_num) * (0.8 + 0.4*Math.random());
+        for (var path_i = 0; path_i < app_path[app].length; path_i++) {
+          const path = app_path[app][path_i];
+          const path_time = (app_time / app_path[app].length) * (0.8 + 0.4*Math.random());
+          if (Math.random() < 0.1)
+            continue;
+          var node =
             {
-              name: "google.com",
-              days: [
+              name: app,
+              days: [],
+              children: [
                 {
-                  datetime: date,
-                  activeseconds: 3600,
-                  semiidleseconds: 3600
+                  name: path,
+                  days: [
+                    {
+                      datetime: date,
+                      activeseconds: path_time,
+                      semiidleseconds: path_time * 0.1 * (0.8 + 0.4*Math.random())
+                    }
+                  ],
+                  children: []
                 }
-              ],
-              children: []
-            }
-          ]
-        };
-      children.push(node);
+              ]
+            };
+          children.push(node);
+
+        }
+      }
     }
-
-
 
     return data;
   };
